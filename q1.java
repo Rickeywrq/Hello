e6ad2dea963312135ae5bd6cc70fbde2d1342c01
Rickeywrq <237357989@qq@com>
2019-10-13 22:03:08 +0800

    第一次尝试IDEA链接上传git

--- q1.java
+++ q1.java
@@ -4,6 +4,9 @@ public class q1 {
     public static void main(String[] args){
         System.out.println(1);
         System.out.println(2);
+        System.out.println(3);
+        System.out.println(4);
+
     }
 
 }
